a3e9dd0671df70d90e33db7f9731cf1e95ded96c
Thien tr <thienms98@gmail.com>
2023-09-25 11:27:54 +0700

    enable cors

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -5,7 +5,6 @@ import {cookies} from 'next/headers'
 
 export async function POST (req: NextRequest){
   const accessToken = req.headers.get('cookie')?.slice(6,);
-  console.log(accessToken)
   if(!accessToken) return NextResponse.json({success: false, message: 'Unauthorization'});
 
   if(!process.env.SECRET_KEY_AC) return NextResponse.json({success: false, statusCode: 'env error'});
