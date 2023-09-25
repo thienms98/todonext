a7a4a8f33464071371003d4223352af43c384896
Thien tr <thienms98@gmail.com>
2023-09-25 17:43:31 +0700

    enable cors across middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,15 +15,15 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  const requestHeaders = request.headers
-  requestHeaders.set('Access-Control-Allow-Origin', '*')
-  requestHeaders.set("Access-Control-Allow-Credentials", "true"),
-  requestHeaders.set("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT"),
-  requestHeaders.set("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
-
-  return NextResponse.next({
-    headers: requestHeaders
-  })
+  const res = NextResponse.next()
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  return res
   // return NextResponse.next()
 }
 
