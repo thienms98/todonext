89183712ec10338249f57579a8b3cf0fac66c108
Thien tr <thienms98@gmail.com>
2023-09-27 09:33:12 +0700

    dynamic page error

--- src/app/tasks/[taskId]/page.tsx
+++ src/app/tasks/[taskId]/page.tsx
@@ -1,43 +1,31 @@
-"use server";
+"use client";
 
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import { Task } from "@/utils/types";
-import { headers } from "next/headers";
+import { Task as TaskType } from "@/utils/types";
 import axios from "axios";
-import { notification } from "antd";
-// import { notification } from "antd";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 
-async function getTask(taskId: string, token?: string) {
-  if(!token) return null
-  const {data} = await axios.get(
-    `/api/tasks/${taskId}`,
-    {headers: {'cookie': `token=${token}`}}
-  ) as { data: { success: boolean; message?: string; task?: Task } };
-  return data;
-}
 
-export default async function Task({ params }: { params: { taskId: string } }) {
-  const { taskId } = params;
-  
-  const token = headers().get("cookie")?.slice(6);
-  
-  const data = await getTask(taskId, token);
-  if(!data) { return null }
-  const {success, message, task} = data
-  
-  console.log(task)
+export default function Task() {
+  const {taskId} = useParams()
+  const [task, setTask] = useState<TaskType>()
+  useEffect(() => {
+    axios(`/api/tasks/${taskId}`).then(({data}) => setTask(data.task))
+  })
+
   return (
     <>
       <Head>
-        {/* <title>Todo - {task.title}</title> */}
+        <title>Todo - {task?.title}</title>
       </Head>
       <div
-        className="fixed w-screen h-screen bg-slate-700/50 z-50"
+        className="fixed w-screen h-screen z-50"
         // onClick={() => router.back()}
       >
-        {task && <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">
+        {task ? <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">
           <div>Title    : {task.title}</div>
           <div>Create At: {new Date(task.createdDate).toLocaleDateString()}</div>
           <div>Due At   : {new Date(task.deadline).toLocaleDateString()}</div>
@@ -49,7 +37,10 @@ export default async function Task({ params }: { params: { taskId: string } }) {
             ))
           }</div></div>
           <div>Done: {task.completed ? 'Yes' : 'No'}</div>
-        </div>}
+        </div>:
+          <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">Task not found 
+          <Link href={'/tasks'}>Back to homepage</Link></div>
+        }
       </div>
     </>
   );
