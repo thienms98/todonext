2d1a047b8eec7c5fa18a9dd7bb7c605051d25c70
Thien tr <thienms98@gmail.com>
2023-09-27 10:49:35 +0700

    fix dynamic task page layout

--- src/app/tasks/[taskId]/page.tsx
+++ src/app/tasks/[taskId]/page.tsx
@@ -7,14 +7,20 @@ import { Task as TaskType } from "@/utils/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import Loading from "@/components/Loading";
 
 
 export default function Task() {
   const {taskId} = useParams()
   const [task, setTask] = useState<TaskType>()
+  const [loading, setLoading] = useState<boolean>(true)
   useEffect(() => {
-    axios(`/api/tasks/${taskId}`).then(({data}) => setTask(data.task))
-  })
+    setLoading(true)
+    axios(`/api/tasks/${taskId}`).then(({data}) => {
+      setTask(data.task);
+      setLoading(false)
+    })
+  }, [taskId])
 
   return (
     <>
@@ -25,22 +31,27 @@ export default function Task() {
         className="fixed w-screen h-screen z-50"
         // onClick={() => router.back()}
       >
-        {task ? <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">
-          <div>Title    : {task.title}</div>
-          <div>Create At: {new Date(task.createdDate).toLocaleDateString()}</div>
-          <div>Due At   : {new Date(task.deadline).toLocaleDateString()}</div>
-          <div>Creator  : <div className="inline-block w-6 h-6 rounded-full overflow-hidden"><Image src={task.creator.image || ''} width={24} height={24} alt='' title={task.creator.username} /></div></div>
-          <div>Assignees: <div className="inline-flex">
-          {
-            task.assignees.map(assignee => (
-              <div key={assignee.id} className="w-6 h-6 rounded-full overflow-hidden"><Image src={task.creator.image || ''} width={24} height={24} alt='' title={task.creator.username} /></div>
-            ))
-          }</div></div>
-          <div>Done: {task.completed ? 'Yes' : 'No'}</div>
-        </div>:
-          <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">Task not found 
-          <Link href={'/tasks'}>Back to homepage</Link></div>
-        }
+        <Loading loading={loading} >
+          {task ? <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">
+            <div>Title    : {task.title}</div>
+            <div>Create At: {new Date(task.createdDate).toLocaleDateString()}</div>
+            <div>Due At   : {new Date(task.deadline).toLocaleDateString()}</div>
+            <div>Creator  : <div className="inline-block w-6 h-6 rounded-full overflow-hidden"><Image src={task.creator.image || ''} width={24} height={24} alt='' title={task.creator.username} /></div></div>
+            <div>Assignees: <div className="inline-flex">
+            {
+              task.assignees && task.assignees.length > 0 ?
+              task.assignees.map(assignee => (
+                <div key={assignee.id} className="w-6 h-6 rounded-full overflow-hidden"><Image src={task.creator.image || ''} width={24} height={24} alt='' title={task.creator.username} /></div>
+              ))
+              : "No"
+            }
+            </div></div>
+            <div>Done: {task.completed ? 'Yes' : 'Not yet'}</div>
+          </div>:
+            <div className="min-w-[50%] min-h-[50%] py-10 px-20 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white">Task not found 
+            <Link href={'/tasks'}>Back to homepage</Link></div>
+          }
+        </Loading>
       </div>
     </>
   );
